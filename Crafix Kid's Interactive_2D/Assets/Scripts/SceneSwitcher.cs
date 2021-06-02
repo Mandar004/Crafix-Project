e38f92a66620c8df57834c3e348920d18b6a49c9
Moskov <mandar.kaware.3@gmail.com>
2021-06-02 14:13:07 +0530

    added Game scenes

--- Crafix Kid's Interactive_2D/Assets/Scripts/SceneSwitcher.cs
+++ Crafix Kid's Interactive_2D/Assets/Scripts/SceneSwitcher.cs
@@ -5,13 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class SceneSwitcher : MonoBehaviour
 {
-    public void GoToGame()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-    }
-    public void GoToHome()
-    {
+    public void GoToHome()  => SceneManager.LoadScene(0);
+    public void Game1()     => SceneManager.LoadScene(1);
+    public void Game2()     => SceneManager.LoadScene(2);
+    public void Game3()     => SceneManager.LoadScene(3);
+    public void Game4()     => SceneManager.LoadScene(4);
+    public void Game5()     => SceneManager.LoadScene(5);
+    public void Game6()     => SceneManager.LoadScene(6);
+    public void Levels()    => SceneManager.LoadScene(7);
 
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
-    }
 }
